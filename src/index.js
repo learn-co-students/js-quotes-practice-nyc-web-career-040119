dedf2c3788b67f3ad8378350b30d1b2420d662b3
William Sinn <w.h.sinn@gmail.com>
2019-05-21 04:24:03 -0400

    add new-quote deliverable

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const quotesUrl = "http://localhost:3000/quotes"
 
 document.addEventListener('DOMContentLoaded', (event) => {
   const ulQuoteList = document.querySelector("#quote-list")
+  const newQuoteFormBox = document.querySelector("#new-quote-form")
+
+
 
   fetch(quotesUrl).then(resp => resp.json())
     .then(quotesObj => {
@@ -31,8 +34,42 @@ document.addEventListener('DOMContentLoaded', (event) => {
         onClick.parentElement.parentElement.remove()
         fetch(`http://localhost:3000/quotes/${deleteId}`, {method:"DELETE"})
       }
+    })//Delete a quote, works for db
+    newQuoteFormBox.addEventListener("submit", function(event){
+      event.preventDefault()
 
+      let newQuote = document.querySelector("#new-quote")
+      let newAuthor = document.querySelector("#new-author")
+      fetch(quotesUrl, {
+        method:"POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accepts: "application/json",
+        },
+        body: JSON.stringify({
+          "quote": newQuote.value,
+          "author": newAuthor.value,
+          "likes": 0
+        })
+      })
+      .then(resp => resp.json())
+      .then((newQuoteItem) => {
+        console.log(newQuoteItem)
+        ulQuoteList.innerHTML += `
+          <li class='quote-card'>
+            <blockquote class="blockquote">
+              <p class="mb-0">${newQuoteItem.quote}</p>
+              <footer class="blockquote-footer">${newQuoteItem.author}</footer>
+              <br>
+              <button class='btn-success'>Likes: <span id="${newQuoteItem.id}">${newQuoteItem.likes}</span></button>
+              <button class='btn-danger' id="${newQuoteItem.id}">Delete</button>
+            </blockquote>
+          </li>`
+
+      })
+      newQuoteFormBox.reset()
     })
+
 
 
 
